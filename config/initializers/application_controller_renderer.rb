ba4e794a3076ca898898f4e97102a453d7004bb4
vagrant <vagrant@localhost.localdomain>
2020-08-23 08:49:32 +0000

    [finish]3-11

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c93befafacfe3d7e34e1b4a292ddd9ae81c155371912a9ad2a3e0913eb5f6561b790a2b7ad45964a324042ba08d4f4ba450d9583b8046308acf12e355fd5a0dd'
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
 	# 新規投稿
     get 'todolists/new' => 'todolists#new'
 	get 'top' => 'homes#top'
+	get 'todolists' => 'todolists#index'
 	post 'todolists' => 'todolists#create'
-  	get 'todolists' => 'todolists#index'
   	get 'todolists/:id' => 'todolists#show', as: 'todolist'
   	get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  	patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+
+  	delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 
 end
